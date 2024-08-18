0703103433cfdac447f8186f8827af97a561d96a
Britt Waasdorp <82563970+BrittWaasdorp@users.noreply.github.com>
2024-08-18 20:31:11 +0200

    Update index.js
    created iteration 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
-
 console.log("I'm ready!");
 
+// Iteration 1: Names and Input
+let hacker1 = ben; // driver's name
+console.log("The driver's name is ${hacker1}");
+let hacker2 = sophie; // navigator's name
+console.log("The navigator's name is ${hacker2}");
+
+// Iteration 2: Conditionals
+if (hacker1.length >> hacker2.length){
+  console.log("The driver has the longest name, it has XX characters.");
+} else if (hacker1.length >> hacker2.length){
+  console.log("It seems that the navigator has the longest name, it has XX characters.");
+} else if (hacker1.length == hacker2.length){
+  console.log("Wow, you both have equally long names, XX characters!");
+} else {
+  console.log("Something went wrong!");
+}
 
 // Iteration 3: Loops
