2495846c1e054021a4eb3e49a49a85f8ae3a8261
Britt Waasdorp <82563970+BrittWaasdorp@users.noreply.github.com>
2024-08-18 21:08:40 +0200

    Update index.js
    corrected code iteration 1-3.2

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
-let hacker1 = ben; // driver's name
-console.log("The driver's name is ${hacker1}");
-let hacker2 = sophie; // navigator's name
-console.log("The navigator's name is ${hacker2}");
+let hacker1 = "Ben"; // driver's name
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "Sophie"; // navigator's name
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 if (hacker1.length >> hacker2.length){
-  console.log("The driver has the longest name, it has XX characters.");
-} else if (hacker1.length >> hacker2.length){
-  console.log("It seems that the navigator has the longest name, it has XX characters.");
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length << hacker2.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 } else if (hacker1.length == hacker2.length){
-  console.log("Wow, you both have equally long names, XX characters!");
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 } else {
   console.log("Something went wrong!");
 }
@@ -20,14 +20,17 @@ if (hacker1.length >> hacker2.length){
 // Iteration 3: Loops
 
 let SpellName = "";
-for (let i == 0; i < hacker1.length; i++){
-  SpellName += "hacker1[i]";
+for (let i = 0; i < hacker1.length; i++){
+  if (i == hacker1.length - 1){
+    SpellName += `${hacker1[i].toUpperCase()}`; // after last letter add no space
+  } else {
+  SpellName += `${hacker1[i].toUpperCase()} `; // add space between letters
+  }
 }
 console.log(SpellName);
 
 let ReverseName = "";
-for (let i == 0; i < hacker1.length; i++){
-  ReverseName += "hacker1[length - 1 - i]";
+for (let i = 0; i < hacker2.length; i++){
+  ReverseName += hacker2[hacker2.length - 1 - i];
 }
 console.log(ReverseName);
-
